448c54381cd8e0da578c2fda05b78224d4211457
Timothee Ruas <timothee.ruas@ysance.com>
2013-02-05 17:48:15 +0100

    mysql package

--- server.js
+++ server.js
@@ -1,7 +1,71 @@
 
-var http = require('http');
+var _url = require('url');
+var _http = require('http');
+var _mysql = require('node-mysql');
+
 var port = process.env.PORT || 80;
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('ça marche...\n');
-}).listen(port);
+
+handler = function(req, res) {
+    
+    res.setHeader('Content-Type' : 'application/json');
+    allowCrossDomain(req, res);
+    
+    var handled = false;
+
+    //if (req.method === 'GET') {
+
+    path = _url.parse(req.url).pathname.split('/');
+
+    if (path.length >= 2) {
+        switch (path[path.length - 2]) {
+            case 'board':
+                switch (path[path.length - 1]) {
+                    case 'add':
+                        break;
+                    case 'poll':
+                        break;
+                }
+                break;
+            case 'auth':
+                switch () {
+                    case 'identify':
+                        handled = true;
+
+                        var client = new _mysql.Client();
+                        client.user = 'root';
+                        client.password = '';
+                        client.connect();
+                        
+                        var connection = mysql.createConnection(os.environ["MYSQLCONNSTR_APP"]);
+                        connection.connect();
+
+                        connection.query('SELECT * FROM user;', function(err, rows, fields) {
+                            if (err) throw err;
+                            res.statusCode = 200;
+                            var body = {};
+                            body.sucess = true;
+                            body.rows = rows;
+                            res.end(JSON.stringify(body));
+                        });
+
+                        connection.end();
+
+                        break;
+                }
+                break;
+        }
+    }
+
+    if (!handled) {
+        res.statusCode = 400;
+        res.end(JSON.stringify({success: false}));
+    }
+};
+
+var allowCrossDomain = function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+}
+
+_http.createServer(handler).listen(port);
