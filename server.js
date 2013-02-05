49bf391aae6c4c6f8679399b9df2797fc7ca3f67
Timothee Ruas <timothee.ruas@ysance.com>
2013-02-05 18:24:05 +0100

    tests..

--- server.js
+++ server.js
@@ -3,14 +3,8 @@ var _url = require('url');
 var _http = require('http');
 
 var handler = function (req, res) {
-
-    res.setHeader('Content-Type' : 'application/json');
-
-    var handled = false;
-
-    res.statusCode = 200;
+    res.writeHead(200, {'Content-Type' : 'application/json'});
     res.write(JSON.stringify({success: false}));
-
     res.end();
 };
 
