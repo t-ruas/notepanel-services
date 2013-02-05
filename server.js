1379907b9f8e6b09e7d66e5d0a91b3f73be1694c
Timothee Ruas <timothee.ruas@ysance.com>
2013-02-05 18:01:19 +0100

    tests..

--- server.js
+++ server.js
@@ -10,49 +10,9 @@ handler = function(req, res) {
     
     var handled = false;
 
-    //if (req.method === 'GET') {
+    res.statusCode = 200;
+    res.end(JSON.stringify({success: false}));
 
-    path = _url.parse(req.url).pathname.split('/');
-
-    if (path.length >= 2) {
-        switch (path[path.length - 2]) {
-            case 'board':
-                switch (path[path.length - 1]) {
-                    case 'add':
-                        break;
-                    case 'poll':
-                        break;
-                }
-                break;
-            case 'auth':
-                switch (path[path.length - 1]) {
-                    case 'identify':
-                        handled = true;
-
-                        var connection = _mysql.createConnection(os.environ["MYSQLCONNSTR_APP"]);
-                        connection.connect();
-
-                        connection.query('SELECT * FROM user;', function(err, rows, fields) {
-                            if (err) throw err;
-                            res.statusCode = 200;
-                            var body = {};
-                            body.sucess = true;
-                            body.rows = rows;
-                            res.end(JSON.stringify(body));
-                        });
-
-                        connection.end();
-
-                        break;
-                }
-                break;
-        }
-    }
-
-    if (!handled) {
-        res.statusCode = 400;
-        res.end(JSON.stringify({success: false}));
-    }
 };
 
 var allowCrossDomain = function (req, res) {
