92bfbcc99723da7e9a8271dce16d011c50b9f611
Timothee Ruas <timothee.ruas@ysance.com>
2013-02-05 18:15:16 +0100

    tests..

--- server.js
+++ server.js
@@ -2,23 +2,14 @@
 var _url = require('url');
 var _http = require('http');
 
+var handler = function (req, res) {
 
-handler = function(req, res) {
-    
     res.setHeader('Content-Type' : 'application/json');
-    allowCrossDomain(req, res);
-    
+
     var handled = false;
 
     res.statusCode = 200;
     res.end(JSON.stringify({success: false}));
-
 };
-
-var allowCrossDomain = function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-}
 
 _http.createServer(handler).listen(80);
