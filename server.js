f00471081171a6aaa24849173c7eee8c90d3abdf
Timothee Ruas <timothee.ruas@ysance.com>
2013-02-05 18:38:01 +0100

    tests..

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 
-var http = require('http');
-
-var handler = function (req, res) {
-    res.setHeader('Content-Type' : 'application/json');
-    res.statusCode = 200;
-    res.end(JSON.stringify({success: false}));
-};
-
-http.createServer(handler).listen(process.env.PORT);
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
