48b9e179ef21dca7e5b5580c061a938815606e23
Timothee Ruas <timothee.ruas@ysance.com>
2013-02-06 17:46:46 +0100

    data module & note editing

---
+++ data.js
@@ -0,0 +1,93 @@
+﻿var _mysql = require('mysql');
+var _server = require('./server.js');
+
+var parseConnectionString = function (str) {
+    var parts = str.split(';');
+    var obj = {};
+    for (var i = 0, imax = parts.length; i < imax; i++) {
+        var kv = parts[i].split('=');
+        obj[kv[0]] = kv[1];
+    }
+    return obj;
+};
+
+var getMySqlConnection = function () {
+    var cs = parseConnectionString(_server.settings.connectionString);
+    return _mysql.createConnection({
+        host: cs["Data Source"],
+        user: cs["User Id"],
+        password: cs["Password"],
+        database: cs["Database"]
+    });
+};
+
+var getUserById = function (cnx, id, callback) {
+    cnx.query('SELECT id, email, name FROM user WHERE id = ?;',
+            [id],
+        function(error, rows, fields) {
+            if (error) {
+                callback({text: 'sql error', inner: error});
+            } else {
+                callback(null, rows.length > 0 ? rows[0] : null);
+            }
+        });
+};
+
+var getUserByNameAndPassword = function (cnx, name, password, callback) {
+    cnx.query('SELECT id, email, name FROM user WHERE name = ? AND password = MD5(?);',
+            [name, password],
+        function(error, rows, fields) {
+            if (error) {
+                callback({text: 'sql error', inner: error});
+            } else {
+                callback(null, rows.length > 0 ? rows[0] : null);
+            }
+        });
+};
+
+var listBoardsByUserId = function (cnx, userId, callback) {
+    cnx.query('SELECT b.id, b.name FROM board AS b JOIN board_user AS bu ON bu.board_id = b.id AND bu.user_id = ?;',
+            [userId],
+        function(error, rows, fields) {
+            if (error) {
+                callback({text: 'sql error', inner: error});
+            } else {
+                callback(null, rows);
+            }
+        });
+};
+
+var saveNote = function (cnx, note, callback) {
+    console.log('saving note [%s]', note.id);
+    if (!note.id) {
+        console.log('insert');
+        cnx.query('INSERT INTO note (board_id, text, x, y, color, creation_date, edition_date) VALUES (?, ?, ?, ?, ?, NOW(), NOW());',
+                [note.boardId, note.text, note.x, note.y, note.color],
+            function(error, result) {
+                console.dir(result);
+                if (error) {
+                    callback({text: 'sql error', inner: error});
+                } else {
+                    callback(null, result.insertId);
+                }
+            });
+    } else {
+        console.log('update');
+        cnx.query('UPDATE note SET text = ?, x = ?, y = ?, color = ?, edition_date = NOW() WHERE id = ?;',
+                [note.text, note.x, note.y, note.color, note.id],
+            function(error, result) {
+                console.dir(result);
+                if (error) {
+                    callback({text: 'sql error', inner: error});
+                } else {
+                    callback(null, note.id);
+                }
+            });
+    }
+};
+
+exports.getMySqlConnection = getMySqlConnection;
+exports.getUserById = getUserById;
+exports.getUserByNameAndPassword = getUserByNameAndPassword;
+exports.listBoardsByUserId = listBoardsByUserId;
+exports.saveNote = saveNote;
--- server.js
+++ server.js
@@ -1,109 +1,182 @@
-
 var _url = require('url');
 var _http = require('http');
-var _mysql = require('mysql');
 var _cookies = require('cookies');
+var _keygrip = require('keygrip');
+var _data = require('./data.js');
+var _querystring = require('querystring');
 
-var parseConnectionString = function (str) {
-    var parts = str.split(';');
-    var obj = {};
-    for (var i = 0, imax = parts.length; i < imax; i++) {
-        var kv = parts[i].split('=');
-        obj[kv[0]] = kv[1];
-    }
-    return obj;
+var settings = {
+    connectionString: process.env['MYSQLCONNSTR_notepanel'] || 'Data Source=localhost;User Id=root;Password=;Database=notepanel',
+    port: process.env['PORT'] || 5001,
+    secrets: [
+        'jhgjfgjgfjgfjj',
+        'vfvcvwxcvwxcvwxcv'
+    ]
 };
 
-var getMySqlConnection = function () {
-    var cs = parseConnectionString(process.env["MYSQLCONNSTR_notepanel"]);
-    return _mysql.createConnection({
-        host: cs["Data Source"],
-        user: cs["User Id"],
-        password: cs["Password"],
-        database: cs["Database"]
+var setCurrentUserId = function (cookies, userId) {
+    cookies.set('notepanel_services_user', userId, {signed: true});
+};
+
+var getCurrentUserId = function (cookies) {
+    return cookies.get('notepanel_services_user', {signed: true});
+};
+
+var listener = function (request, response) {
+
+    context = {
+        request: request,
+        response: response,
+        path: _url.parse(request.url, true),
+        cookies: new _cookies(request, response, _keygrip(settings.secrets)),
+        body: ''
+    };
+
+    request.addListener('data', function (chunk) {
+        context.body += chunk;
+    });
+    
+    request.addListener("end", function () {
+        
+        response.setHeader('Content-Type', 'application/json');
+        response.setHeader('Access-Control-Allow-Origin', '*');
+        response.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+        response.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+        if (context.body.length > 0) {
+            context.content = _querystring.parse(context.body);
+        }
+        
+        handleRequest(context);
     });
 };
 
-var handler = function (req, res) {
-
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
+var handleRequest = function (context) {
     var handled = false;
-    res.statusCode = 200;
     
-    path = _url.parse(req.url, true).pathname.split('/');
+    var routes = [
+        {pattern: /^\/users\/login$/g, method: 'GET', handler: onUsersLogin},
+        {pattern: /^\/users\/logout$/g, method: 'GET', handler: onUsersLogout},
+        {pattern: /^\/users\/identify$/g, method: 'GET', handler: onUsersIdentify},
+        {pattern: /^\/boards\/poll$/g, method: 'GET', handler: onBoardsPoll},
+        {pattern: /^\/notes$/g, method: 'POST', handler: onNotes}
+    ];
 
-    if (path.length > 0) {
-        switch (path[0]) {
-            case 'boards':
-                if (path.length > 1) {
-                    switch (path[1]) {
-                        case 'poll':
-                            break;
-                    }
+    for (var i = 0, imax = routes.length; i < imax; i++) {
+        if (context.path.pathname.match(routes[i].pattern) && routes[i].method === context.request.method) {
+            routes[i].handler(context, function (error, result) {
+                if (error) {
+                    context.response.statusCode = 500;
+                    context.response.write(JSON.stringify({success: false, message: error}));
+                } else {
+                    context.response.statusCode = 200;
+                    context.response.write(JSON.stringify({success: true, message: result}));
                 }
-                break;
-            case 'users':
-                if (path.length > 1) {
-                    switch (path[1]) {
-                        case 'login':
-                            handled = true;
-                            var body = {};
-                            var cnx = getMySqlConnection();
-                            cnx.connect();
-                            cnx.query(
-                                'SELECT id, email, name FROM user WHERE name = ? AND MD5(password) = ?;',
-                                [path.query.username, path.query.password],
-                                function(err, rows, fields) {
-                                    if (err) {
-                                        body.success = false;
-                                    } else {
-                                        body.success = true;
-                                        if (rows.length > 0) {
-                                            body.identified = true;
-                                            body.user = rows[0];
-                                            _cookies.set('notepanel_services_user', body.user['id'], {signed: true});
-                                        }
-                                    }
-                                    res.end(JSON.stringify(body));
-                                });
-                            cnx.end();
-                            break;
-                        case 'identify':
-                            handled = true;
-                            var body = {};
-                            var cookie = _cookies.get('notepanel_services_user', {signed: true});
-                            if (cookie) {
-                                var cnx = getMySqlConnection();
-                                cnx.connect();
-                                cnx.query('SELECT id, email, name FROM user WHERE id = ?;', [cookie], function(err, rows, fields) {
-                                    if (err) {
-                                        body.success = false;
-                                    } else {
-                                        body.success = true;
-                                        if (rows.length > 0) {
-                                            body.identified = true;
-                                            body.user = rows[0];
-                                        }
-                                    }
-                                    res.end(JSON.stringify(body));
-                                });
-                                cnx.end();
-                            }
-                            break;
-                    }
-                }
-                break;
+                context.response.end();
+            });
+            handled = true;
+            break;
         }
     }
 
     if (!handled) {
-        res.statusCode = 404;
-        res.end();
+        context.response.statusCode = 404;
+        context.response.end();
     }
 };
 
-_http.createServer(handler).listen(process.env.PORT || 5001);
+var onUsersLogin = function (context, callback) {
+    var cnx = _data.getMySqlConnection();
+    cnx.connect();
+    _data.getUserByNameAndPassword(cnx, context.path.query.username, context.path.query.password,
+        function(error, result) {
+            if (error) {
+                cnx.end();
+                callback(error);
+            } else {
+                if (result) {
+                    setCurrentUserId(context.cookies, result['id']);
+                    var message = {identified: true, user: result};
+                    _data.listBoardsByUserId(cnx, message.user.id,
+                        function(error, result) {
+                            if (error) {
+                                callback(error);
+                            } else {
+                                message.boards = result;
+                                callback(null, message);
+                            }
+                        });
+                    cnx.end();
+                } else {
+                    cnx.end();
+                    callback(null, {identified: false});
+                }
+            }
+        });
+};
+
+var onUsersLogout = function (context, callback) {
+    var userId = getCurrentUserId(context.cookies);
+    if (userId) {
+        setCurrentUserId(context.cookies);
+    }
+    callback(null, {identified: false});
+};
+
+var onUsersIdentify = function (context, callback) {
+    var userId = getCurrentUserId(context.cookies);
+    if (userId) {
+        var cnx = _data.getMySqlConnection();
+        cnx.connect();
+        _data.getUserById(cnx, userId,
+            function(error, result) {
+                if (error) {
+                    cnx.end();
+                    callback(error);
+                } else {
+                    if (result) {
+                        var message = {identified: true, user: result};
+                        _data.listBoardsByUserId(cnx, message.user.id,
+                            function(error, result) {
+                                if (error) {
+                                    callback(error);
+                                } else {
+                                    message.boards = result;
+                                    callback(null, message);
+                                }
+                            });
+                        cnx.end();
+                    } else {
+                        cnx.end();
+                        callback(null, {identified: false});
+                    }
+                }
+                
+            });
+    } else {
+        callback(null, {identified: false});
+    }
+};
+
+var onNotes = function (context, callback) {
+    var cnx = _data.getMySqlConnection();
+    cnx.connect();
+    _data.saveNote(cnx, context.content,
+        function(error, result) {
+            if (error) {
+                callback(error);
+            } else {
+                callback(null, {id: result});
+            }
+        });
+    cnx.end();
+};
+
+var onBoardsPoll = function (context, callback) {
+    var message = {};
+    callback(null, message);
+};
+
+_http.createServer(listener).listen(settings.port);
+
+exports.settings = settings;
